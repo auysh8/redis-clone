3095da552ab66e966094ae9619ec0a9acd0512d2
auysh8 <auysh1993@gmail.com>
2026-09-05 12:23:14 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,17 @@
+import type { Buffer } from "buffer";
+import { connect } from "http2";
 import * as net from "net";
 
-
 const server: net.Server = net.createServer((connection: net.Socket) => {
-    connection.write("+PONG\r\n")
+  connection.on("data", (data: Buffer) => {
+    const message = data.toString().split("\r\n");
+    console.log(message);
+    if (message.includes("PING")) {
+      connection.write("+PONG\r\n");
+    } else if (data.includes("ECHO")) {
+      connection.write(`$${message[4].length}\r\n${message[4]}\r\n`);
+    }
+  });
 });
 
 server.listen(6379, "127.0.0.1");
