42afffb9a407f06f180b381bc914854cd8e53f43
auysh8 <auysh1993@gmail.com>
2026-09-05 13:06:48 +0530

    Implement basic Redis commands: PING, ECHO, SET, and GET with database support

--- app/main.ts
+++ app/main.ts
@@ -1,15 +1,27 @@
 import type { Buffer } from "buffer";
-import { connect } from "http2";
 import * as net from "net";
+
+const database = new Map<string, string>();
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
   connection.on("data", (data: Buffer) => {
     const message = data.toString().split("\r\n");
     console.log(message);
-    if (message.includes("PING")) {
+    const command = message[2]?.toUpperCase();
+    if (command === "PING") {
       connection.write("+PONG\r\n");
-    } else if (data.includes("ECHO")) {
+    } else if (command === "ECHO") {
       connection.write(`$${message[4].length}\r\n${message[4]}\r\n`);
+    } else if (command === "SET") {
+      database.set(message[4], message[6]);
+      connection.write("+OK\r\n");
+    } else if (command === "GET") {
+      const value = database.get(message[4]);
+      if (value == undefined) {
+        connection.write("$-1\r\n");
+      } else {
+        connection.write(`$${value?.length}\r\n${value}\r\n`);
+      }
     }
   });
 });
