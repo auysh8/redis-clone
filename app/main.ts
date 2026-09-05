6ea15e074c46ad7c6b8855a0e5ca6ae8b3b92c53
auysh8 <auysh1993@gmail.com>
2026-09-05 13:38:53 +0530

    Refactor database structure to support expiration for SET and GET commands

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,7 @@
 import type { Buffer } from "buffer";
 import * as net from "net";
 
-const database = new Map<string, string>();
+const database = new Map<string, { value: string; expiresAt?: number }>();
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
   connection.on("data", (data: Buffer) => {
@@ -13,14 +13,24 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
     } else if (command === "ECHO") {
       connection.write(`$${message[4].length}\r\n${message[4]}\r\n`);
     } else if (command === "SET") {
-      database.set(message[4], message[6]);
+      const subCommand = message[8]?.toUpperCase() || null;
+      const key = message[4];
+      const value = message[6];
+      if (subCommand === "PX") {
+        const expiresAt = Date.now() + Number(message[10]);
+        database.set(key, { value, expiresAt });
+      } else {
+        database.set(key, { value });
+      }
       connection.write("+OK\r\n");
     } else if (command === "GET") {
-      const value = database.get(message[4]);
-      if (value == undefined) {
+      const data = database.get(message[4]);
+      if (!data) {
+        connection.write("$-1\r\n");
+      } else if (data.expiresAt && data.expiresAt < Date.now()) {
         connection.write("$-1\r\n");
       } else {
-        connection.write(`$${value?.length}\r\n${value}\r\n`);
+        connection.write(`$${data.value?.length}\r\n${data.value}\r\n`);
       }
     }
   });
