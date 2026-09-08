92c976fad1255cb592025e8b676979a3448a87bc
auysh8 <auysh1993@gmail.com>
2026-09-08 13:48:03 +0530

    Refactor command handling to use a command map for improved structure and readability

--- app/main.ts
+++ app/main.ts
@@ -4,6 +4,7 @@ import * as net from "net";
 const stringStore = new Map<string, { value: string; expiresAt?: number }>();
 const listStore = new Map<string, string[]>();
 
+// For parsing RESP commands
 const parseRESP = (data: Buffer) => {
   const dataArr = data.toString().split("\r\n");
   const tokens = [];
@@ -17,51 +18,58 @@ const parseRESP = (data: Buffer) => {
   return commandTokens;
 };
 
-const server: net.Server = net.createServer((connection: net.Socket) => {
-  connection.on("data", (data: Buffer) => {
-    const commandTokens = parseRESP(data);      //convert the RESP command to something usefull
-    console.log(commandTokens);
-    const command = commandTokens.command;      //extract the command from command tokens
-    
-    if (command === "PING") {
-      connection.write("+PONG\r\n");
-    } else if (command === "ECHO") {
-      connection.write(`$${commandTokens.args[0].length}\r\n${commandTokens.args[0]}\r\n`);
-    } else if (command === "SET") {
-      const subCommand = commandTokens.args[2]?.toUpperCase() || null;
-      const key = commandTokens.args[0];
-      const value = commandTokens.args[1];
-      if (subCommand === "PX") {
-        const expiresAt = Date.now() + Number(commandTokens.args[3]);
-        stringStore.set(key, { value, expiresAt });
-      } else {
-        stringStore.set(key, { value });
-      }
-      connection.write("+OK\r\n");
-    } else if (command === "GET") {
-      const data = stringStore.get(commandTokens.args[0]);
-      if (!data) {
-        connection.write("$-1\r\n");
-      } else if (data.expiresAt && data.expiresAt < Date.now()) {
-        connection.write("$-1\r\n");
-      } else {
-        connection.write(`$${data.value?.length}\r\n${data.value}\r\n`);
-      }
-    } else if (command === "RPUSH") {
-      const key = commandTokens.args[0];
-      const value: string[] = [];
-      for (let i = 1; i < commandTokens.args.length; i++) {
-        value.push(commandTokens.args[i]);
-      }
-      if (listStore.has(key)) {
-        listStore.get(key)?.push(...value);
-      } else {
-        listStore.set(key, value);
-      }
-      const lenOfList = listStore.get(key)?.length || 0;
-      connection.write(`:${lenOfList}\r\n`);
-    } else if (command === "LRANGE") {
-      const key = commandTokens.args[0];
+//contains all the commands and thier callback functions
+const commandMap: Record<
+  string,
+  (connection: net.Socket, args: string[]) => void
+> = {
+  PING: (connection) => connection.write("+PONG\r\n"),
+
+  ECHO: (connection, args) =>
+    connection.write(`$${args[0].length}\r\n${args[0]}\r\n`),
+
+  SET: (connection, args) => {
+    const subCommand = args[2]?.toUpperCase() || null;
+    const key = args[0];
+    const value = args[1];
+    if (subCommand === "PX") {
+      const expiresAt = Date.now() + Number(args[3]);
+      stringStore.set(key, { value, expiresAt });
+    } else {
+      stringStore.set(key, { value });
+    }
+    connection.write("+OK\r\n");
+  },
+
+  GET: (connection, args) => {
+    const data = stringStore.get(args[0]);
+    if (!data) {
+      connection.write("$-1\r\n");
+    } else if (data.expiresAt && data.expiresAt < Date.now()) {
+      connection.write("$-1\r\n");
+    } else {
+      connection.write(`$${data.value?.length}\r\n${data.value}\r\n`);
+    }
+  },
+
+  RPUSH: (connection, args) => {
+    const key = args[0];
+    const value: string[] = [];
+    for (let i = 1; i < args.length; i++) {
+      value.push(args[i]);
+    }
+    if (listStore.has(key)) {
+      listStore.get(key)?.push(...value);
+    } else {
+      listStore.set(key, value);
+    }
+    const lenOfList = listStore.get(key)?.length || 0;
+    connection.write(`:${lenOfList}\r\n`);
+  },
+
+  LRANGE: (connection, args) => {
+    {
+      const key = args[0];
       let returnValue = "";
       let countItems = 0;
       if (listStore.has(key)) {
@@ -69,28 +77,52 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
         let startRange = 0;
         let endRange = 0;
         const listLen = list.length;
-        if (Number(commandTokens.args[1]) < 0) {
-          startRange = listLen + Number(commandTokens.args[1]);
+        if (Number(args[1]) < 0) {
+          startRange = listLen + Number(args[1]);
           if (startRange < 0) {
             startRange = 0;
           }
         } else {
-          startRange = Number(commandTokens.args[1]);
+          startRange = Number(args[1]);
         }
-        if (Number(commandTokens.args[2]) < 0) {
-          endRange = listLen + Number(commandTokens.args[2]);
+        if (Number(args[2]) < 0) {
+          endRange = listLen + Number(args[2]);
         } else {
-          endRange = Math.min(Number(commandTokens.args[2]), listLen - 1);
+          endRange = Math.min(Number(args[2]), listLen - 1);
         }
-        console.log(endRange, "end");
         for (let i = startRange; i <= endRange; i++) {
           returnValue += "$" + list[i].length + "\r\n" + list[i] + "\r\n";
           countItems++;
         }
       }
-      console.log(`*${countItems + "\r\n" + returnValue}`);
       connection.write(`*${countItems}\r\n${returnValue}`);
     }
+  },
+};
+
+const server: net.Server = net.createServer((connection: net.Socket) => {
+  connection.on("data", (data: Buffer) => {
+    const commandTokens = parseRESP(data); //convert the RESP command to something usefull
+    const command: string = commandTokens.command; //extract the command from command tokens
+
+    // To run commands
+    const runCommand = (
+      command: string,
+      commandMap: Record<
+        string,
+        (connection: net.socket, args: string[]) => void
+      >,
+    ) => {
+      const commandHandler = commandMap[command];
+
+      if (commandHandler) {
+        commandHandler(connection, commandTokens.args);
+      } else {
+        connection.write(`-ERROR unknow command${command}\r\n`);
+      }
+    };
+
+    runCommand(command, commandMap);
   });
 });
 
