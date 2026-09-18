0943c6aaf138919751818f49b7fb980f47c1d1a1
auysh8 <auysh1993@gmail.com>
2026-09-18 15:39:20 +0530

    Add listCommands implementation and integrate with commandMap

--- app/command/index.ts
+++ app/command/index.ts
@@ -1,9 +1,11 @@
 import * as net from "net";
 import { streamCommands } from "./streamCommands";
+import { listCommands } from "./listCommands";
 
 const commandMap: Record<
   string,
   (connection: net.Socket, args: string[]) => void
 > = {
   ...streamCommands,
+  ...listCommands,
 };
